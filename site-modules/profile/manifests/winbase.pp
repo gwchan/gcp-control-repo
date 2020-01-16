b63ed637a1f1570fad56d424457badd5f68c0d1e
puppet <admin@gcp.internal>
2020-01-16 22:41:08 +0800

    Amend password

--- site-modules/profile/manifests/winbase.pp
+++ site-modules/profile/manifests/winbase.pp
@@ -3,6 +3,6 @@ class profile::winbase {
   #the base profile should include component modules that will be on all windows nodes
   user {'test1':
     ensure => present,
-    password => 'password123',
+    password => 'P@ssw0rd123',
   }
 }
