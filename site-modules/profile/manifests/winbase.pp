3536b364fefe7fdbfe965855174e9993d348a7dc
puppet <admin@gcp.internal>
2020-01-16 10:24:40 +0800

    Added Test User to WinBase

--- site-modules/profile/manifests/winbase.pp
+++ site-modules/profile/manifests/winbase.pp
@@ -1,5 +1,8 @@
 class profile::winbase {
 
   #the base profile should include component modules that will be on all windows nodes
-  
+  dsc_user {'Test1':
+    dsc_ensure => present,
+    dsc_disabled => false,
+  }
 }
