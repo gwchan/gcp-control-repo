b472cc99045bdba4dfb259144688b85589af05aa
puppet <admin@gcp.internal>
2020-01-18 22:47:59 +0800

    Amend User Accounts in Windows

--- site-modules/profile/manifests/winbase.pp
+++ site-modules/profile/manifests/winbase.pp
@@ -1,11 +1,26 @@
 class profile::winbase {
 
   #the base profile should include component modules that will be on all windows nodes
+  user { 'Administrator':
+    ensure  => 'present',
+    comment => 'Built-in account for administering the computer/domain',
+    groups  => ['BUILTIN\Administrators'],
+  }
+
+  user { 'DefaultAccount':
+    ensure  => 'present',
+    comment => 'A user account managed by the system.',
+    groups  => ['BUILTIN\System Managed Group'],
+  }
+  
   user {'Guest':
     name => 'Guest',
     ensure => present,
     groups    => ['Guests'],
-    managehome => true,
     comment => 'Built-in account for guest access to the computer/domain (Managed by Puppet)',
+  }
+
+  exec { 'Disable Guest Account':
+    command     => 'net user Guest /active:no',
   }
 }
