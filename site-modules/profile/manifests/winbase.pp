7458d1ddf1a7f42ddb2dbcd683d2dbfc00e4fcbb
puppet <admin@gcp.internal>
2020-01-18 22:29:41 +0800

    Amend Guest User

--- site-modules/profile/manifests/winbase.pp
+++ site-modules/profile/manifests/winbase.pp
@@ -2,6 +2,10 @@ class profile::winbase {
 
   #the base profile should include component modules that will be on all windows nodes
   user {'Guest':
+    name => 'Guest',
     ensure => present,
+    groups    => ['Guests'],
+    managehome => true,
+    comment => 'Built-in account for guest access to the computer/domain (Managed by Puppet)',
   }
 }
