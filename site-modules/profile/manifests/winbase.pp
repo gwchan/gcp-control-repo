40a313f38d165e4da2ddab796a605c77c9ff4656
puppet <admin@gcp.internal>
2020-01-18 22:20:47 +0800

    Manage Guest User

--- site-modules/profile/manifests/winbase.pp
+++ site-modules/profile/manifests/winbase.pp
@@ -1,8 +1,7 @@
 class profile::winbase {
 
   #the base profile should include component modules that will be on all windows nodes
-  user {'test1':
+  user {'Guest':
     ensure => present,
-    password => 'P@ssw0rd123',
   }
 }
