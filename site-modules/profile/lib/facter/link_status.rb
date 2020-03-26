d07bf76dcf498304b3a0784d4dae5aac882248a3
puppet <admin@gcp.internal>
2020-03-26 14:08:12 +0800

    Remove unecessary loop

--- site-modules/profile/lib/facter/link_status.rb
+++ site-modules/profile/lib/facter/link_status.rb
@@ -9,16 +9,13 @@ Facter.add('link_status') do
           #:encoding => 'utf-8'
           file_out = File.read(file_name).split("\n")
           
-          file_out.each do |line|
-            #puts line
-            item  = line.tr('"', '').split(',')
-            link_status[item[0]] = {
+          item  = file_out[0].tr('"', '').split(',')
+          link_status[item[0]] = {
               'Status'     => item[1].gsub(/True/,'Up'),
               'Speed' => (item[2].to_i / 1000000000).to_s + ' Gbps',
               'MacAddress' => item[3],
               'Name' => item[4]
-            }
-          end
+          }
         end
         # Clear Temp File
         clear_file = Facter::Core::Execution.exec('C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy remotesigned -Command "& { rm \"C:\Temp\link_status.txt\" }"')  
