a5001829cf06b41615903bbf63375d7adde5eda1
puppet <admin@gcp.internal>
2020-05-27 09:51:05 +0800

    Modify PS syntax

--- site-modules/profile/lib/facter/win_cluster.rb
+++ site-modules/profile/lib/facter/win_cluster.rb
@@ -3,7 +3,7 @@ Facter.add('Cluster Owner') do
   confine osfamily: :windows
   cowner = 'NA'
 
-  cowner = Facter::Core::Execution.execute('powershell "Get-ClusterGroup -Name "Cluster Group" | Select -ExpandProperty "OwnerNode" | Select -ExpandProperty "Name""')
+  cowner = Facter::Core::Execution.execute('powershell "(Get-ClusterGroup -Name "Cluster Group" | Select -ExpandProperty "OwnerNode" | Select -ExpandProperty "Name")"')
   
   setcode do
     cowner  
