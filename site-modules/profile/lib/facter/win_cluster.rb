dfd6d945435fde48194e7c566364a19e83038052
puppet <admin@gcp.internal>
2020-05-27 19:42:57 +0800

    Add Is Owner facts

--- site-modules/profile/lib/facter/win_cluster.rb
+++ site-modules/profile/lib/facter/win_cluster.rb
@@ -9,3 +9,17 @@ Facter.add('Cluster Owner') do
     cowner  
   end
 end
+
+Facter.add('is_cluster_owner') do
+  confine osfamily: :windows
+  cowner = 'NA'
+  result = 'No'
+
+  cowner = Facter::Core::Execution.execute('powershell "(Get-ClusterGroup -Name \"Cluster Group\" | Select -ExpandProperty \"OwnerNode\" | Select -ExpandProperty \"Name\")"')
+  if Facter.value(:hostname) == cowner
+    result = 'Yes'
+
+  setcode do
+    cowner  
+  end
+end
