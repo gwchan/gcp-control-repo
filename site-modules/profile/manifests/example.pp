98234b630515bf08c8208fe418e96728b5667e60
guangwei <gw@gcp.internal>
2020-07-26 13:34:20 +0000

    change path again

--- site-modules/profile/manifests/example.pp
+++ site-modules/profile/manifests/example.pp
@@ -6,6 +6,6 @@ class profile::example {
   }
   file { '/tmp/display.html':
     ensure => file,
-    content => epp('display.epp', {'nodes_result' => $debian_nodes}),
+    content => epp('templates/display.epp', {'nodes_result' => $debian_nodes}),
   }
 }
