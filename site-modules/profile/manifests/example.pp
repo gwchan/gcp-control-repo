924885226b1c3454aa58f2770106294205d41332
guangwei <gw@gcp.internal>
2020-07-26 13:59:44 +0000

    formating

--- site-modules/profile/manifests/example.pp
+++ site-modules/profile/manifests/example.pp
@@ -1,5 +1,10 @@
 class profile::example {
-  $debian_nodes_query = 'nodes[certname]{facts{name = "operatingsystem" and value = "CentOS"}}'
+  $debian_nodes_query = 'nodes[certname] {
+                          facts {
+                            name = "operatingsystem" 
+                            and value = "CentOS"
+                          }
+                        }'
   $debian_nodes = puppetdb_query($debian_nodes_query).map |$value| { $value["certname"] }
   notify {"Debian nodes":
     message => "Your debian nodes are ${join($debian_nodes, ', ')}",
