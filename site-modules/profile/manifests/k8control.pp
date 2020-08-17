7eec4a87b70902559319ef7963d139181d513e0d
guangwei <gw@gcp.internal>
2020-08-17 09:03:00 +0000

    Added pre-requisites

--- site-modules/profile/manifests/k8control.pp
+++ site-modules/profile/manifests/k8control.pp
@@ -10,7 +10,29 @@ class profile::k8control {
     recurse => true,
   }
 
+  file { '/home/guangwei/.kube':
+    ensure  => directory,
+    mode    => '0600',
+    recurse => true,
+    owner   => 'guangwei',
+    group   => 'guangwei',
+  }
+
   class {'kubernetes':
     controller => true,
+  }
+
+  exec {'copykubeconfig':
+    command => 'sudo cp -i /etc/kubernetes/admin.conf /home/guangwei/.kube/config',
+    user    => 'guangwei',
+    require => Class['kubernetes'],
+  }
+
+  file { '/home/guangwei/.kube/config':
+    ensure  => present,
+    mode    => '0600',
+    owner   => 'guangwei',
+    group   => 'guangwei',
+    require => Exec ['copykubeconfig'],
   }
 }
